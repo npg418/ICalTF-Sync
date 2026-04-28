2d353c0cdd2fcc03cddbdf96bcb32ffaf42d36aa
NPG418 <narupenguin919@gmail.com>
2026-04-28 10:20:12 +0900

    chore: neovim環境を整備

---
+++ flake.nix
@@ -0,0 +1,83 @@
+{
+  description = "Project flake for ICalTF-Sync";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    systems.url = "github:nix-systems/default";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    my-nixvim = {
+      url = "github:npg418/dotfiles?dir=nixvim";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        nixvim.follows = "nixvim";
+        flake-parts.follows = "flake-parts";
+      };
+    };
+  };
+
+  outputs =
+    inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.nixvim.flakeModules.default
+      ];
+      nixvim = {
+        packages.enable = true;
+        checks.enable = true;
+      };
+      flake.nixvimModules.default = {
+        lsp.servers = {
+          nil_ls.enable = true;
+          ts_ls.enable = true;
+          biome = {
+            enable = true;
+            packageFallback = true;
+          };
+        };
+        plugins = {
+          conform-nvim = {
+            autoInstall.overrides = {
+              biome-check = null;
+            };
+            settings = {
+              formatters-by-ft = {
+                nix = [ "nixfmt" ];
+                javascript = [ "biome-check" ];
+                typescript = [ "biome-check" ];
+              };
+            };
+          };
+        };
+      };
+      perSystem =
+        {
+          self',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          nixvimConfigurations.default = inputs.nixvim.lib.evalNixvim {
+            inherit system;
+            modules = [
+              inputs.my-nixvim.nixvimModules.default
+              self.nixvimModules.default
+            ];
+          };
+          devShells.default = pkgs.mkShellNoCC {
+            packages = [
+              self'.packages.default
+              pkgs.bun
+            ];
+          };
+        };
+    };
+}
